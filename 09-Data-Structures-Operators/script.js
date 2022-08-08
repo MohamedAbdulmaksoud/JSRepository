39494662d7d2309e709c74f32155d3ad2373bfea
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2022-08-08 02:09:57 +0200

    Sets

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -44,15 +44,46 @@ const restaurant = {
 };
 
 /***************************
- * LOOPING OBJECTS
+ * SETS
  ***************************/
-//property NAMES
-const properties = Object.keys(openingHours);
-console.log(properties);
+const ordersSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+console.log(ordersSet);
+console.log(new Set("Jonas"));
+console.log(ordersSet.size);
+console.log(ordersSet.has("Pizza"));
+console.log(ordersSet.has("Bread"));
+ordersSet.add("Garlic Bread");
+ordersSet.add("Garlic Bread");
+ordersSet.delete("Risotto");
+// ordersSet.clear();
+console.log(ordersSet);
+for (const order of ordersSet) console.log(order);
+// Example
+const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
+);
+console.log(new Set("jonasschmedtmann").size);
 
-//property VALUES
-const values = Object.values(openingHours);
-console.log(values);
+// /***************************
+//  * LOOPING OBJECTS
+//  ***************************/
+// //property NAMES
+// const properties = Object.keys(openingHours);
+// console.log(properties);
+
+// //property VALUES
+// const values = Object.values(openingHours);
+// console.log(values);
 
 //Entire Objects
 const entries = Object.entries(openingHours);
