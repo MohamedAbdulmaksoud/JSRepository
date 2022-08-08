cd80aa1aa6cec54b1886ff7270f6c91d526b5f72
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2022-08-09 00:01:33 +0200

    Reference vs. Value

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,24 +1,45 @@
 "use strict";
-// Default Parameters
-const bookings = [];
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  // ES5
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+// /***************************
+//  * REFERENCE VS. PRIMITAIVE
+//  ***************************/
+const flight = "LH123";
+const passenger = {
+  name: "Yasser",
+  passportNumber: "A27055694",
 };
-createBooking("LH123");
-createBooking("LH123", 2, 800);
-createBooking("LH123", 2);
-createBooking("LH123", 5);
-createBooking("LH123", undefined, 1000);
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH987";
+  passenger.name = "Mr. " + passenger.name;
+  if (passenger.passportNumber === "A27055694") alert("Checking In");
+  else alert("Wrong Passport!");
+};
+checkIn(flight, passenger);
+console.log(flight);
+console.log(passenger);
+
+// /***************************
+//  * DEFAULT PARAMETERS
+//  ***************************/
+// const bookings = [];
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+// createBooking("LH123");
+// createBooking("LH123", 2, 800);
+// createBooking("LH123", 2);
+// createBooking("LH123", 5);
+// createBooking("LH123", undefined, 1000);
