e568929e2890dd29548adc53ac6c11041b93d685
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2022-08-11 01:06:49 +0200

    Method Chaining

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -202,29 +202,45 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // );
 // console.log(movementsDescriptions);
 
-/***************************
- * FILTER METHOD
- *************************/
-const arrayWithdrawal = movements.filter((mov) => mov < 0);
-console.log(arrayWithdrawal);
+// /***************************
+//  * FILTER METHOD
+//  *************************/
+// const arrayWithdrawal = movements.filter((mov) => mov < 0);
+// console.log(arrayWithdrawal);
+
+// /***************************
+//  * REDUCE METHOD
+//  *************************/
+// console.log(movements);
+// // accumulator -> SNOWBALL
+// // const balance = movements.reduce(function (acc, cur, i, arr) {
+// //   console.log(`Iteration ${i}: ${acc}`);
+// //   return acc + cur;
+// // }, 0);
+// const balance = movements.reduce((acc, cur) => acc + cur, 0);
+// console.log(balance);
+// let balance2 = 0;
+// for (const mov of movements) balance2 += mov;
+// console.log(balance2);
+// // Maximum value
+// const max = movements.reduce((acc, mov) => {
+//   if (acc > mov) return acc;
+//   else return mov;
+// }, movements[0]);
+// console.log(max);
 
 /***************************
- * REDUCE METHOD
+ * METHOD CHAINING
  *************************/
+const eurToUsd = 1.1;
 console.log(movements);
-// accumulator -> SNOWBALL
-// const balance = movements.reduce(function (acc, cur, i, arr) {
-//   console.log(`Iteration ${i}: ${acc}`);
-//   return acc + cur;
-// }, 0);
-const balance = movements.reduce((acc, cur) => acc + cur, 0);
-console.log(balance);
-let balance2 = 0;
-for (const mov of movements) balance2 += mov;
-console.log(balance2);
-// Maximum value
-const max = movements.reduce((acc, mov) => {
-  if (acc > mov) return acc;
-  else return mov;
-}, movements[0]);
-console.log(max);
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter((mov) => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
