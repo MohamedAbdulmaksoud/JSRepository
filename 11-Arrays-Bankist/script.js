e945fa83eb265793e4ead5cd8de4247ac29ec0d4
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2022-08-10 14:25:54 +0200

    createUsernames fcn: forRach and Map

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -81,7 +81,16 @@ const displayMovements = function (movements, sort = false) {
     containerMovements.insertAdjacentHTML("afterbegin", html);
   });
 };
-
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
+createUsernames(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -144,24 +153,28 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //   }
 // });
 
+// /***************************
+//  * MAP METHOD
+//  *************************/
+// const eurToUsd = 1.1;
+// // const movementsUSD = movements.map(function (mov) {
+// //   return mov * eurToUsd;
+// // });
+
+// const movementsUSD = movements.map((mov) => mov * eurToUsd);
+// console.log(movements);
+// console.log(movementsUSD);
+// const movementsUSDfor = [];
+// for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+// console.log(movementsUSDfor);
+// const movementsDescriptions = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
+//       mov
+//     )}`
+// );
+// console.log(movementsDescriptions);
+
 /***************************
  * MAP METHOD
  *************************/
-const eurToUsd = 1.1;
-// const movementsUSD = movements.map(function (mov) {
-//   return mov * eurToUsd;
-// });
-
-const movementsUSD = movements.map((mov) => mov * eurToUsd);
-console.log(movements);
-console.log(movementsUSD);
-const movementsUSDfor = [];
-for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
-console.log(movementsUSDfor);
-const movementsDescriptions = movements.map(
-  (mov, i) =>
-    `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
-      mov
-    )}`
-);
-console.log(movementsDescriptions);
