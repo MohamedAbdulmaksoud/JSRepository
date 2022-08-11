f9dd76d24f2e2b7469317158a380f49a1aa72ed6
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2022-08-12 01:04:41 +0200

    remainder and numertic separators

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -284,32 +284,67 @@ btnSort.addEventListener("click", function (e) {
 // console.log(Number.isInteger(23.0));
 // console.log(Number.isInteger(23 / 0));
 
-// Math and Rounding
-console.log(Math.sqrt(25));
-console.log(25 ** (1 / 2));
-console.log(8 ** (1 / 3));
-console.log(Math.max(5, 18, 23, 11, 2));
-console.log(Math.max(5, 18, "23", 11, 2));
-console.log(Math.max(5, 18, "23px", 11, 2));
-console.log(Math.min(5, 18, 23, 11, 2));
-console.log(Math.PI * Number.parseFloat("10px") ** 2);
-console.log(Math.trunc(Math.random() * 6) + 1);
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min) + 1) + min;
-// 0...1 -> 0...(max - min) -> min...max
-// console.log(randomInt(10, 20));
-// Rounding integers
-console.log(Math.round(23.3));
-console.log(Math.round(23.9));
-console.log(Math.ceil(23.3));
-console.log(Math.ceil(23.9));
-console.log(Math.floor(23.3));
-console.log(Math.floor("23.9"));
-console.log(Math.trunc(23.3));
-console.log(Math.trunc(-23.3));
-console.log(Math.floor(-23.3));
-// Rounding decimals
-console.log((2.7).toFixed(0));
-console.log((2.7).toFixed(3));
-console.log((2.345).toFixed(2));
-console.log(+(2.345).toFixed(2));
+// // Math and Rounding
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
+// console.log(Math.max(5, 18, 23, 11, 2));
+// console.log(Math.max(5, 18, "23", 11, 2));
+// console.log(Math.max(5, 18, "23px", 11, 2));
+// console.log(Math.min(5, 18, 23, 11, 2));
+// console.log(Math.PI * Number.parseFloat("10px") ** 2);
+// console.log(Math.trunc(Math.random() * 6) + 1);
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min) + 1) + min;
+// // 0...1 -> 0...(max - min) -> min...max
+// // console.log(randomInt(10, 20));
+// // Rounding integers
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
+// console.log(Math.floor(23.3));
+// console.log(Math.floor("23.9"));
+// console.log(Math.trunc(23.3));
+// console.log(Math.trunc(-23.3));
+// console.log(Math.floor(-23.3));
+// // Rounding decimals
+// console.log((2.7).toFixed(0));
+// console.log((2.7).toFixed(3));
+// console.log((2.345).toFixed(2));
+// console.log(+(2.345).toFixed(2));
+
+// The Remainder Operator
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+console.log(6 % 2);
+console.log(6 / 2);
+console.log(7 % 2);
+console.log(7 / 2);
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+labelBalance.addEventListener("click", function () {
+  [...document.querySelectorAll(".movements__row")].forEach(function (row, i) {
+    // 0, 2, 4, 6
+    if (i % 2 === 0) row.style.backgroundColor = "orangered";
+    // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = "blue";
+  });
+});
+///////////////////////////////////////
+// Numeric Separators
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+const price = 345_99;
+console.log(price);
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+const PI = 3.1415;
+console.log(PI);
+console.log(Number("230_000"));
+console.log(parseInt("230_000"));
