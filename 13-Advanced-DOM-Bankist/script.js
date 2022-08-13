ff308db6097f1795878d48f553a735a197d39d36
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2022-08-13 02:16:44 +0200

    styles, attributes and classes

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -41,22 +41,22 @@ document.addEventListener("keydown", function (e) {
 /***************************
  * SELECTING, CREATING AND DELETING ELEMENTS
  ***************************/
-console.log("document.documentElement: \n", document.documentElement);
-console.log("document.head: \n", document.head);
-console.log("document.body: \n", document.body);
+// console.log("document.documentElement: \n", document.documentElement);
+// console.log("document.head: \n", document.head);
+// console.log("document.body: \n", document.body);
 
 const header = document.querySelector(".header");
 const allSections = document.querySelectorAll(".section");
-console.log("document.querySelectorAll('.section'): \n", allSections);
+// console.log("document.querySelectorAll('.section'): \n", allSections);
 
 document.getElementById("section--1");
 const allButtons = document.getElementsByTagName("button");
-console.log("document.getElementsByTagName('button')", allButtons);
+// console.log("document.getElementsByTagName('button')", allButtons);
 
-console.log(
-  "document.getElementsByClassName('btn')",
-  document.getElementsByClassName("btn")
-);
+// console.log(
+//   "document.getElementsByClassName('btn')",
+//   document.getElementsByClassName("btn")
+// );
 
 //Creating and inserting elements
 const message = document.createElement("div");
@@ -73,7 +73,43 @@ header.before(message);
 
 //Deleting elements
 document
-  .querySelector("btn--close-cookie")
+  .querySelector(".btn--close-cookie")
   .addEventListener("click", function () {
     message.remove();
   });
+
+/***************************
+ * STYLES, ATTRIBUTES AND CLASSES
+ ***************************/
+
+//Styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+console.log("message.style.width = ", message.style.width);
+//does not return anything because not set as it is inline style
+console.log("message.style.height = ", message.style.height);
+//Get the non-fetchtable CSS styles
+console.log(
+  "getComputedStyle(message).color = ",
+  getComputedStyle(message).color
+);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+//Attributes
+const logo = document.querySelector(".nav__logo");
+//Standard Attributes reading
+console.log("logo.alt = ", logo.alt);
+console.log("logo.src = ", logo.src);
+//Non-Standard Attributes Reading
+console.log(logo.getAttribute("designer"));
+
+const link = document.querySelector(".nav__link--btn");
+console.log("link.href = ", link.href); //output: http://127.0.0.1:8080/13-Advanced-DOM-Bankist/#
+console.log("link.getAttribute('href') = ", link.getAttribute("href")); //output: #
+
+//Data attributes
+console.log(logo.dataset.versionNumber);
