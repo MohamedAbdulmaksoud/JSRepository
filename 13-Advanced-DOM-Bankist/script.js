d80ee1e37a818ef24b8ae15672f96ed6cea6e975
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2022-08-13 05:22:10 +0200

    Tabbed component

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -3,7 +3,6 @@
  * PROJECT
  ***************************/
 ///////////////////////////////////////
-// Modal window
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
@@ -11,7 +10,9 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
-
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 //Modal Window
 const openModal = function (e) {
   e.preventDefault();
